748ce76f45c6bbca317366a88d9b6ee0cbf5d485
Davydov Artyom <fax1ty.ru@gmail.com>
2022-10-17 21:43:38 +0300

    Open site on localhost instead of 127.0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 import autoprefixer from "autoprefixer";
+import dns from "dns";
 import postcssLogical from "postcss-logical";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import { swcReactRefresh } from "vite-plugin-swc-react-refresh";
 
+dns.setDefaultResultOrder("verbatim");
+
 export default defineConfig({
   server: {
+    host: "localhost",
     port: 3000,
   },
   plugins: [swcReactRefresh(), svgr(), eslintPlugin()],
