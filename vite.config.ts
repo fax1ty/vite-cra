af1d548149a150d32bc90a56ee564e8056cfbf93
Artemiy Davydov <artemiy.davydov@mercurydevelopment.com>
2023-01-16 20:49:41 +0300

    Vite 4! Hooray!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import eslintPlugin from "@nabla/vite-plugin-eslint";
+import swcReactRefresh from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
 import dns from "dns";
 import postcssLogical from "postcss-logical";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import { swcReactRefresh } from "vite-plugin-swc-react-refresh";
 
 dns.setDefaultResultOrder("verbatim");
 
