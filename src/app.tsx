52ba23eab68dc26e21f7f077173152b063ced9f5
Artemiy Davydov <artemiy.davydov@mercurydevelopment.com>
2023-06-26 21:38:22 +0300

    Keeping everything up-to-date

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+export const App = () => {
+  return (
+    <>
+      <p
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translateX(-50%) translateY(-50%)",
+        }}
+      >
+        Everything is working!
+      </p>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,10 @@ import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { App } from "./app";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div>
-      <h1>You can edit it right now!</h1>
-      <h2>It's shipped out of the box 🥡 with</h2>
-      <ul>
-        <li>SWC for Typescript compilation</li>
-        <li>SASS (with modules)</li>
-        <li>React Fast Refresh</li>
-        <li>SVG Components (including SVG optimization)</li>
-        <li>Auto-prefixer</li>
-        <li>ESLint and Prettier</li>
-      </ul>
-    </div>
+    <App />
   </React.StrictMode>
 );
